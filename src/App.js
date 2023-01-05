1c59f9b9ce188dc96f7acce70e87aad5654222df
divyajyothi22 <divyajyothineyyila@gmail.com>
2023-01-05 22:02:44 +0530

    final Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from './pages';
+import Header from "./components/header.js";
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header/>
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
